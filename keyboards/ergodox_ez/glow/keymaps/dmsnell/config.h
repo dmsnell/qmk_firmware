870b2596a3054748239d5a7e1b37a86f2531f5fe
Dennis Snell <dennis.snell@automattic.com>
2021-01-05 12:53:51 -0700

    Add ergodox_ez/glow/dmsnell

---
+++ keyboards/ergodox_ez/glow/keymaps/dmsnell/config.h
@@ -0,0 +1,13 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#define MOUSEKEY_WHEEL_MAX_SPEED 6
+
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 60
+
+#define FIRMWARE_VERSION u8"q9yO3/Bwe6x"
+#define RGB_MATRIX_STARTUP_SPD 60
